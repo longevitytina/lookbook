2ab0777925f9f597ead7a26656d1a3d0dd18a0af
Tina <longevity.tina@gmail.com>
2021-05-03 16:51:16 -0700

    Cleans up code

--- src/Components/carousel.js
+++ src/Components/carousel.js
@@ -7,37 +7,41 @@ import SLIDES from "./App/slides";
 
 function CarouselSlides() {
   return (
-    <>
-      <Carousel
-        interval={10000}
-        style={{ maxWidth: "1200px", margin: "20px auto" }}
-      >
-        <Carousel.Item className="image-one">
-          <img
-            className="d-block w-100"
-            src={SLIDES[0].image_url}
-            alt="First slide"
-          />
-          <div className="hotspot-one">product_id: 102021-404</div>
-        </Carousel.Item>
-        <Carousel.Item>
-          <img
-            className="d-block w-100"
-            src={SLIDES[1].image_url}
-            alt="Second slide"
-          />
-          <div className="hotspot-two">product_id: 102022-651</div>
-        </Carousel.Item>
-        <Carousel.Item>
-          <img
-            className="d-block w-100"
-            src={SLIDES[2].image_url}
-            alt="Third slide"
-          />
-          <div className="hotspot-three">product_id: 102011-410</div>
-        </Carousel.Item>
-      </Carousel>
-    </>
+    <Carousel
+      interval={10000}
+      style={{ maxWidth: "1200px", margin: "20px auto" }}
+    >
+      <Carousel.Item className="image-one">
+        <img
+          className="d-block w-100"
+          src={SLIDES[0].image_url}
+          alt="First slide"
+        />
+        <div className="hotspot-one">
+          Product: {SLIDES[0].hotspots.product_id}
+        </div>
+      </Carousel.Item>
+      <Carousel.Item>
+        <img
+          className="d-block w-100"
+          src={SLIDES[1].image_url}
+          alt="Second slide"
+        />
+        <div className="hotspot-two">
+          Product: {SLIDES[1].hotspots.product_id}
+        </div>
+      </Carousel.Item>
+      <Carousel.Item>
+        <img
+          className="d-block w-100"
+          src={SLIDES[2].image_url}
+          alt="Third slide"
+        />
+        <div className="hotspot-three">
+          Product: {SLIDES[2].hotspots.product_id}
+        </div>
+      </Carousel.Item>
+    </Carousel>
   );
 }
 
